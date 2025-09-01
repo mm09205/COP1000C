0b0d76338ecb293709fd8f26de4e3bf7a4e61ad6
mm09205 <michaelmelendez0922@gmail.com>
2025-08-31 23:17:35 -0400

    Add files via upload

---
+++ VariableFun.py
@@ -0,0 +1,16 @@
+
+answer_1 = 5 * 6
+print("The answer to your query is:", answer_1)
+answer_2 = 8 / 3
+print("The answer to your query is:", answer_2)
+answer_3 = 8 // 3 #without remainder
+print("The answer to your query is:", answer_3)
+answer_4 = 8 % 3 #modulo
+print("The answer to your query is:", answer_4)
+answer_5 = 4200000 + 23090
+print("The answer to your query is:", answer_5)
+answer_6 = 4200000 - 7633
+print("The answer to your query is:", answer_6)
+
+print("Michael Melendez")
+
